6282ca0399d25e9cc0336d7a74f84dfa44be2365
taureus <pythonprogram767@gmail.com>
2022-09-18 23:46:25 +0530

    login

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function clicks(){
-    document.getElementsByClassName('home').style='display:block;'
-    return
-}   
+
+
+
+
+
+function get() {
+    console.log("hjghjn");
+    let id=document.getElementById('username').value;
+let pswd=document.getElementById('password');
+    console.log(id.value);
+    console.log(pswd.value);
+   
+}

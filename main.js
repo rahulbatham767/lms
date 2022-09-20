dc878844ee454c0487ad680d9fe7d1d8e902cdc7
taureus <pythonprogram767@gmail.com>
2022-09-21 00:02:33 +0530

    login page

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 
+// let id=document.getElementById('username').value;
 
-
-
-
-function get() {
-    console.log("hjghjn");
-    let id=document.getElementById('username').value;
-let pswd=document.getElementById('password');
-    console.log(id.value);
-    console.log(pswd.value);
-   
-}
+let names=document.forms['form1']["username"].value;
+console.log(names);
